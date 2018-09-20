20de6975aacf8914cbd96173f703650e4ea82db8
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-09-20 21:04:16 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<html>
-<body>
-       <iframe id="playeriframe" name="playeriframe" src="https://players-saranyu.s3.amazonaws.com/saranyu_players_demo/index_embed_drm.html?contenturl=https://saranyucctv.tk/live/blr_test1.stream/manifest.mpd?usendtime=1537369744&ushash=eKETE7qJZGRZQzTDGcpJ_x9QIHoZcnf2ypk-Ji3-q2U=&widevine_url=true&playready_url=true" width="704" height="576"> </iframe>
-</body>
-
-</html>
-
+<?php 
+header( 'Location: /index.html' ) ;  
+?>
